a95d630d57c150e8f0ff37209fda31ec1415b90e
Colton Clemmer <coltonclemmerdev@gmail.com>
2023-02-17 15:45:29 -0600

    Read prune config file

--- src/bag_of_words.rs
+++ src/bag_of_words.rs
@@ -1,8 +1,9 @@
 use itertools::Itertools;
-use std::{collections::HashMap, ops::Index, io::{Write, Read}};
+use std::{collections::HashMap, ops::Index, io::{Write, Read}, str::FromStr, fmt::Debug};
 use rand::Rng;
 use std::time::Instant;
 use std::fs::File;
+use json::{parse, JsonValue};
 
 use crate::util::{InputTup, multi_thread_process_list, get_percent, reduce};
 
@@ -11,6 +12,15 @@ use crate::util::{InputTup, multi_thread_process_list, get_percent, reduce};
 pub type WordBag = (usize, HashMap<String, f32>);
 pub type BagMap = HashMap<String, WordBag>;
 
+fn get_json<T>(obj: &JsonValue, c: &str, k: &str, def: T) -> T where T: FromStr, <T as FromStr>::Err: Debug {
+    let err_s = "Error parsing: ".to_owned() + c.clone() + "-" + k.clone();
+    if obj.has_key(k) {
+        obj[k].dump().parse::<T>().expect(&err_s)
+    } else {
+        def
+    }
+}
+
 pub struct PruneProbabilityConfig {
     // Starts at this probability
     pub starting_probability: f32,
@@ -18,6 +28,25 @@ pub struct PruneProbabilityConfig {
     pub max_accuracy_reduction: f32,
     // Will multiply the minimum probability by this number every iteration
     pub probability_multiplyer: f32,
+}
+
+impl PruneProbabilityConfig {
+    pub fn default() -> PruneProbabilityConfig {
+        PruneProbabilityConfig { 
+            starting_probability: 0.00001,
+            max_accuracy_reduction: 0.1,
+            probability_multiplyer: 2.0
+        }
+    }
+
+    pub fn from_json(obj: &JsonValue) -> PruneProbabilityConfig {
+        let mut tmp_config = PruneProbabilityConfig::default();
+        let probability_s = "probability";
+        tmp_config.starting_probability = get_json(obj, probability_s, "starting_probability", tmp_config.starting_probability);
+        tmp_config.starting_probability = get_json(obj, probability_s, "max_accuracy_reduction", tmp_config.max_accuracy_reduction);
+        tmp_config.starting_probability = get_json(obj, probability_s, "probability_multiplyer", tmp_config.probability_multiplyer);
+        tmp_config
+    }
 }
 
 pub struct PruneSimilarityConfig {
@@ -29,9 +58,45 @@ pub struct PruneSimilarityConfig {
     pub probability_multiplyer: f32
 }
 
+impl PruneSimilarityConfig {
+    pub fn default() -> PruneSimilarityConfig {
+        PruneSimilarityConfig { 
+            starting_deviation: 0.00000001, 
+            max_accuracy_reduction: 0.1, 
+            probability_multiplyer: 2.0
+        }
+    }
+
+    pub fn from_json(obj: &JsonValue) -> PruneSimilarityConfig {
+        let mut tmp_config = PruneSimilarityConfig::default();
+        let similarity_s = "similarity";
+        tmp_config.starting_deviation = get_json(obj, similarity_s, "starting_deviation", tmp_config.starting_deviation);
+        tmp_config.max_accuracy_reduction = get_json(obj, similarity_s, "max_accuracy_reduction", tmp_config.max_accuracy_reduction);
+        tmp_config.probability_multiplyer = get_json(obj, similarity_s, "probability_multiplyer", tmp_config.probability_multiplyer);
+        tmp_config
+    }
+}
+
 pub struct PruneCountConfig {
     pub min_count: i32,
     pub adjust_amount: f32
+}
+
+impl PruneCountConfig {
+    pub fn default() -> PruneCountConfig {
+        PruneCountConfig { 
+            min_count: 2,
+            adjust_amount: 0.01
+        }
+    }
+
+    pub fn from_json(obj: &JsonValue) -> PruneCountConfig {
+        let mut tmp_config = PruneCountConfig::default();
+        let count_s = "count";
+        tmp_config.min_count = get_json::<i32>(obj, count_s, "min_count", tmp_config.min_count);
+        tmp_config.adjust_amount = get_json(obj, count_s, "adjust_amount", tmp_config.adjust_amount);
+        tmp_config
+    }
 }
 
 #[derive(Clone)]
@@ -41,11 +106,51 @@ pub struct RandomizerConfig {
     pub iterations: i32
 }
 
+impl RandomizerConfig {
+    pub fn default() -> RandomizerConfig {
+        RandomizerConfig { 
+            num_params: 10, 
+            step_size: 0.001,
+            iterations: 1000
+        }
+    }
+
+    pub fn from_json(obj: &JsonValue) -> RandomizerConfig {
+        let mut tmp_config = RandomizerConfig::default();
+        let rand_s = "randomizer";
+        tmp_config.num_params = get_json(obj, rand_s, "num_params", tmp_config.num_params);
+        tmp_config.step_size = get_json(obj, rand_s, "step_size", tmp_config.step_size);
+        tmp_config.num_params = get_json(obj, rand_s, "iterations", tmp_config.iterations);
+        tmp_config
+    }
+}
+
 pub struct PruneSelectionConfig {
     pub probability: bool,
     pub similarity: bool,
     pub count: bool,
     pub randomizer: bool
+}
+
+impl PruneSelectionConfig {
+    pub fn default() -> PruneSelectionConfig {
+        PruneSelectionConfig { 
+            probability: false, 
+            similarity: false, 
+            count: false, 
+            randomizer: false 
+        }
+    }
+
+    pub fn from_json(obj: &JsonValue) -> PruneSelectionConfig {
+        let mut tmp_config = PruneSelectionConfig::default();
+        let select_s = "selection";
+        tmp_config.probability = get_json(obj, select_s, "probability", false);
+        tmp_config.similarity = get_json(obj, select_s, "similarity", false);
+        tmp_config.count = get_json(obj, select_s, "count", false);
+        tmp_config.randomizer = get_json(obj, select_s, "randomizer", false);
+        tmp_config
+    }
 }
 
 pub struct LearnConfig {
@@ -248,6 +353,91 @@ impl BagOfWords {
             bm.insert(bag_name, (bag_total, words));
         }
         BagOfWords { bags: bm }
+    }
+
+    /*
+    Config file structure:
+    if the config has a key for the pruning type it is automatically selected to be run
+    if the "selection" object has a key set to true then it will use the default config
+    if the object for a pruning type is missing some elements, then the default config will be used
+    {
+        probability: {
+            starting_probability: 0,
+            max_accuracy_reduction: 0,
+            probability_multiplyer: 0
+        },
+        similarity: {
+            starting_deviation: 0,
+            max_accuracy_reduction: 0,
+            probability_multiplyer: 0
+        },
+        count: {
+            min_count: 0,
+            adjust_amount: 0
+        },
+        randomizer: {
+            num_params: 0,
+            step_size: 0,
+            iterations: 0
+        },
+        selection: {
+            probability: true,
+            similarity: true,
+            count: true,
+            randomizer: true
+        }
+    }
+     */
+
+    pub fn read_config(file_name: &str) -> LearnConfig {
+        let mut config = LearnConfig { 
+            prune_selection: PruneSelectionConfig { 
+                probability: false, 
+                similarity: false, 
+                count: false, 
+                randomizer: false 
+            },
+            prune_probability: None,
+            prune_similarity: None,
+            prune_count: None,
+            randomizer: None
+        };
+        let mut file = File::open(file_name).expect("Creating file object error");
+        let mut file_contents = String::new();
+        file.read_to_string(&mut file_contents).expect("Reading file error");
+        if file_contents.eq("") { panic!("Loading bag of words: File empty") }
+        let json_data = parse(&file_contents).unwrap();
+
+        let probability_s = "probability";
+        if json_data.has_key(probability_s) {
+            config.prune_selection.probability = true;
+            config.prune_probability = Some(PruneProbabilityConfig::from_json(&json_data[probability_s]));
+        }
+
+        let similarity_s = "similarity";
+        if json_data.has_key(similarity_s) {
+            config.prune_selection.similarity = true;
+            config.prune_similarity = Some(PruneSimilarityConfig::from_json(&json_data[similarity_s]));
+        }
+
+        let count_s = "count";
+        if json_data.has_key(count_s) {
+            config.prune_selection.count = true;
+            config.prune_count = Some(PruneCountConfig::from_json(&json_data[count_s]));
+        }
+
+        let randomizer_s = "randomizer";
+        if json_data.has_key(randomizer_s) {
+            config.prune_selection.randomizer = true;
+            config.randomizer = Some(RandomizerConfig::from_json(&json_data[randomizer_s]));
+        }
+
+        let selection_s = "selection";
+        if json_data.has_key(selection_s) {
+            config.prune_selection = PruneSelectionConfig::from_json(&json_data[selection_s]);
+        }
+
+        config
     }
 
     // TODO: decrease probability until there are no removed words, then increase
@@ -500,49 +690,27 @@ impl BagOfWords {
     // , step_size: f32, num_iterations: i32, modify_amount: i32
     pub fn learn(&mut self, input: &Vec<InputTup>, o_learn_config: Option<LearnConfig>) {
         let mut learn_config: LearnConfig;
-        let default_probability_config = PruneProbabilityConfig {
-            starting_probability: 0.00001,
-            max_accuracy_reduction: 0.1,
-            probability_multiplyer: 2.0
-        };
-
-        let default_similarity_config = PruneSimilarityConfig { 
-            starting_deviation: 0.00000001, 
-            max_accuracy_reduction: 0.1, 
-            probability_multiplyer: 2.0
-        };
-
-        let default_randomizer_config = RandomizerConfig { 
-            num_params: 10, 
-            step_size: 0.001,
-            iterations: 1000
-        };
-
-        let default_count_config = PruneCountConfig { 
-            min_count: 2,
-            adjust_amount: 0.01
-        };
 
         if o_learn_config.is_some() {
             learn_config = o_learn_config.expect("Unwrap error");
             if learn_config.prune_probability.is_none() {
-                learn_config.prune_probability = Some(default_probability_config);
+                learn_config.prune_probability = Some(PruneProbabilityConfig::default());
             }
             if learn_config.prune_similarity.is_none() {
-                learn_config.prune_similarity = Some(default_similarity_config);
+                learn_config.prune_similarity = Some(PruneSimilarityConfig::default());
             }
             if learn_config.randomizer.is_none() {
-                learn_config.randomizer = Some(default_randomizer_config);
+                learn_config.randomizer = Some(RandomizerConfig::default());
             }
             if learn_config.prune_count.is_none() {
-                learn_config.prune_count = Some(default_count_config);
+                learn_config.prune_count = Some(PruneCountConfig::default());
             }
         } else {
             learn_config = LearnConfig {
-                prune_probability: Some(default_probability_config),
-                prune_similarity: Some(default_similarity_config),
-                randomizer: Some(default_randomizer_config),
-                prune_count: Some(default_count_config),
+                prune_probability: Some(PruneProbabilityConfig::default()),
+                prune_similarity: Some(PruneSimilarityConfig::default()),
+                randomizer: Some(RandomizerConfig::default()),
+                prune_count: Some(PruneCountConfig::default()),
                 prune_selection: PruneSelectionConfig {
                     probability: false,
                     similarity: false,
